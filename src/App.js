e1fb7ab4f6a5573f7443ccb7c43e48e88cdced19
Kousalya <kousalya@Samirs-MacBook-Pro-2.local>
2026-07-27 17:18:09 +0530

    redirection page changes2

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   const location = useLocation();
 
   // ✅ Added "/" and "/landing_page" to hide the Header on the Landing Page
-  const hideHeaderOn = ["/", "/landing_page", "/admin_login", "/contactus"];
+  const hideHeaderOn = ["/landing_page", "/admin_login", "/contactus"];
 
   const shouldShowHeader = !hideHeaderOn.includes(location.pathname);
 
